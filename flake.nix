671bcc7985b32ebac8c74b1f7ec498cebe3c5bec
friedenberg <1baggzpr@starbrandshoes.com>
2026-05-17 07:55:48 -0400

    bump nixpkgs-master: bb7e5d8 → d233902

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    nixpkgs-master.url = "github:NixOS/nixpkgs/bb7e5d8ac99f4b9d2527f2355e614d6bb0f3288d";
+    nixpkgs-master.url = "github:NixOS/nixpkgs/d233902339c02a9c334e7e593de68855ad26c4cb";
     utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.102";
   };
 
